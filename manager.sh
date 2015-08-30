7860ba2283567a4b1faade81f1045f950f1875c0
Eugene Khashin <adastreamer@gmail.com>
2015-08-31 01:57:35 +0300

    start writing manager.sh

--- manager.sh
+++ manager.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-
+ACTION=$1
+case "$ACTION" in
+"setup_server" )
+  echo "setup_server"
+;;
+"check_server" )
+  echo "check_server"
+;;
+* )
+  echo "Missing or wrong action. Examples:"
+  echo "manager.sh setup_server"
+  echo "manager.sh check_server"
+;;
+esac
