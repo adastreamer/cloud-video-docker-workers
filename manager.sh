2ccafe3e87291f19bad470ca01499e5ceeea7244
Eugene Khashin <adastreamer@gmail.com>
2015-09-01 00:25:31 +0300

    setup_server action started

--- manager.sh
+++ manager.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-ACTION=$1
+ACTION=$1; shift
 case "$ACTION" in
-"setup_server" )
-  echo "setup_server"
-;;
-"check_server" )
-  echo "check_server"
-;;
-* )
-  echo "Missing or wrong action. Examples:"
-  echo "manager.sh setup_server"
-  echo "manager.sh check_server"
-;;
+
+  # SETUP SERVER ACTION
+  "setup_server" )
+    while getopts s:l:p: option
+    do
+      case "${option}" in s) SERVER=${OPTARG};; l) LOGIN=${OPTARG};; p) PASS=${OPTARG};; esac
+    done
+    if [ -z "${SERVER}" ]; then echo "SERVER (-s) is unset or set to the empty string"; exit; fi
+    if [ -z "${LOGIN}" ]; then echo "LOGIN (-l) is unset or set to the empty string"; exit; fi
+    if [ -z "${PASS}" ]; then echo "PASS (-p) is unset or set to the empty string"; exit; fi
+    ssh -t $SERVER bash -c "'
+      docker -v >/dev/null 2>&1 || { echo \"Docker not installed. Exiting...\" >&2; exit; }
+    '"
+  ;;
+
+  # CHECK SERVER ACTION
+  "check_server" )
+    echo "check_server"
+  ;;
+
+  # MISSING ACTION HANDLER
+  * )
+    echo "Missing or wrong action. Examples:"
+    echo "manager.sh setup_server"
+    echo "manager.sh check_server"
+  ;;
+
 esac
