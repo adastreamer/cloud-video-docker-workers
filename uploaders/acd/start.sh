b4d8f9777702a5ef35d0f5390c080a96fb5b8360
Eugene Khashin <adastreamer@gmail.com>
2015-09-02 20:28:42 -0400

    acd startup script

---
+++ uploaders/acd/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "--> start.sh script running..."
+
+echo -e "{"access_token":\"\","exp_time":,"expires_in":,"refresh_token":\"\",\"token_type\":\"bearer\"}" > ~/.cache/acd_cli/oauth_data
